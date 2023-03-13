2bc0c1d7fc7fb42435a3805334b0664541fca144
osama maree <osamamarei2001@gmail.com>
2023-03-13 23:15:38 +0200

    test

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use((err, req, res, next) => {
       .json({ message: "catch error", error: err.message });
   }
 });
+//Test
 app.use("*", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
